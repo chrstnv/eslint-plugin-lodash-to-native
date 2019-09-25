b222c70624afaa7bbbe5dd29a26028631bd419be
Kristina Volosiuk <christinavolosyuk@gmail.com>
2019-09-25 16:18:37 +0300

    Реализован кейс проверки передачи переменной в _.map

--- eslint-plugin-lodash-to-native/lib/rules/map.js
+++ eslint-plugin-lodash-to-native/lib/rules/map.js
@@ -5,30 +5,38 @@
 'use strict';
 
 //------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+const utils = require("eslint-utils")
+
+//------------------------------------------------------------------------------
 // Rule Definition
 //------------------------------------------------------------------------------
 
 module.exports = {
   meta: {
+    type: 'problem',
     docs: {
       description: 'lodash-to-native/map',
-      category: 'Fill me in',
+      category: 'problem',
       recommended: false
     },
-    fixable: null, // or "code" or "whitespace"
+    fixable: 'code', // or "code" or "whitespace"
     schema: [
       // fill in your schema
     ]
   },
-
   create: function(context) {
     // variables should be defined here
+    const message = 'There should be native Array#map here.';
 
     //----------------------------------------------------------------------
     // Helpers
     //----------------------------------------------------------------------
 
     // any helper functions should go here or else delete this section
+    function getVariableType () {
+    }
 
     //----------------------------------------------------------------------
     // Public
@@ -36,11 +44,32 @@ module.exports = {
 
     return {
       "CallExpression[callee.object.name='_'][callee.property.name='map']": node => {
-          context.report({
-            node,
-            message: 'This is it!'
-          });
+        const firstArgumentType = node.arguments[0] && node.arguments[0].type;
 
+        switch (firstArgumentType) {
+          case 'ArrayExpression':
+            context.report({
+              node,
+              message
+            });
+            break;
+
+          case 'Identifier':
+            const firstArgumentName = node.arguments[0] && node.arguments[0].name;
+            const variable = utils.findVariable(context.getScope(), firstArgumentName);
+            const varType = variable.identifiers[0] && variable.identifiers[0].parent.init.type;
+
+            if(varType === 'ArrayExpression') {
+              context.report({
+                node,
+                message
+              });
+            }
+            break;
+
+          default:
+            break;
+        }
       }
     };
   }
--- eslint-plugin-lodash-to-native/tests/lib/rules/map.js
+++ eslint-plugin-lodash-to-native/tests/lib/rules/map.js
@@ -15,20 +15,34 @@ var rule = require('../../../lib/rules/map'),
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester( {'parserOptions': { 'ecmaVersion': 2018 }} );
+var ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2018 } });
 ruleTester.run('map', rule, {
   valid: [
-    // give me some code that won't trigger a warning
-    'let obj = {a : 1, b: 2}; _.map(obj, n => {n * n})'
+    // when an object passed into _.map explicitly
+    '_.map({a : 1, b: 2}, n => {n * n})',
+
+    // when an object variable reference passed into _.map
+    'const obj = {a : 1, b: 2}; _.map(obj, n => {n * n})'
   ],
 
   invalid: [
+    // when an array passed into _.map explicitly
     {
-      code: 'let arr = [1, 2, 3]; _.map(arr, (n) => {n * n})',
+      code: '_.map([1, 2, 3], (n) => {n * n})',
       errors: [
         {
-          message: 'Fill me in.',
-          type: 'Me too'
+          message: 'There should be native Array#map here.',
+          type: 'CallExpression'
+        }
+      ]
+    },
+    // when an array variable reference passed into _.map
+    {
+      code: 'const arr = [1, 2, 3]; _.map(arr, (n) => {n * n})',
+      errors: [
+        {
+          message: 'There should be native Array#map here.',
+          type: 'CallExpression'
         }
       ]
     }
